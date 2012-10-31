f81e0b5174269fd159b4a40c5b431ccfd582b1d0
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2012-10-31 11:50:06 +0400

    Refactored js for DRY

--- javascripts/viewport.js
+++ javascripts/viewport.js
@@ -1,14 +1,15 @@
 (function ($) {
-    $(document).ready(function() {
+    function printWidthHeight() {
         var the_width = $(window).width();
         $('#width').text(the_width);
         var the_height = $(window).height();
         $('#height').text(the_height);
+    }
+
+    $(document).ready(function() {
+        printWidthHeight();
     });
     $(window).resize(function() {
-        var the_width = $(window).width();
-        $('#width').text(the_width);
-        var the_height = $(window).height();
-        $('#height').text(the_height);
+        printWidthHeight();
     });
 })(jQuery);
--- javascripts/youtube-height.js
+++ javascripts/youtube-height.js
@@ -1,10 +1,14 @@
 (function ($) {
-    $(document).ready(function() {
+
+    function resizeYoutubeIframe() {
         var cw = $('.field-name-field-video iframe').width() * 0.7;
         $('..field-name-field-video iframe').css({'height':cw+'px'});
+    }
+
+    $(document).ready(function() {
+        resizeYoutubeIframe();
     });
     $(window).resize(function() {
-        var cw = $('.field-name-field-video iframe').width() * 0.7;
-        $('..field-name-field-video iframe').css({'height':cw+'px'});
+        resizeYoutubeIframe();
     });
 })(jQuery);
