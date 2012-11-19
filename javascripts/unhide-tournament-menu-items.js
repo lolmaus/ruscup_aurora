44120c5009074f3ccbc554c5817716aeef03d1e6
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2012-11-19 23:51:09 +0400

    Added new anchors to the tournament items list

--- javascripts/unhide-tournament-menu-items.js
+++ javascripts/unhide-tournament-menu-items.js
@@ -1,7 +1,10 @@
 (function ($) {
     $.each(["field-name-field-place-reference",
             "field-name-field-judges-view",
-            "field-name-field-timetable"], function(index, value) {
+            "field-name-field-timetable",
+            "field-name-field-future-dates",
+            "field-name-field-categories-view",
+            "field-name-field-participants-view"], function(index, value) {
         if ($("." + value).length > 0) $("#link-"+value).css('display','inline');
     });
 })(jQuery);
